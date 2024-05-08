158eb79d246ae95aa5bc93c82cd7a494f6be57e2
Michał Itrych <michal.itrych1312@gmail.com>
2024-05-09 01:03:21 +0200

    added config.json handling

--- app.py
+++ app.py
@@ -1,16 +1,21 @@
 from flask import Flask
 from flask_cors import CORS
-
-from src.routes.dashboard import dashboard_bp
-from src.routes.offers import offers_bp
-from src.routes.news import news_bp
+import json
 
 app = Flask(__name__)
 CORS(app)
+
+with open('config.json', 'r') as f:
+    config = json.load(f)
+
+# Register blueprints
+from src.routes.dashboard import dashboard_bp
+from src.routes.offers import offers_bp
+from src.routes.news import news_bp
 
 app.register_blueprint(offers_bp)
 app.register_blueprint(news_bp)
 app.register_blueprint(dashboard_bp)
 
 if __name__ == "__main__":
-    app.run()
+    app.run(host=config['host'], port=config['port'], debug=config['debug'])
