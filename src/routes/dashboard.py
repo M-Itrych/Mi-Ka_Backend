5d3bd702c87cf1ff7c88d41adab4ba3df6ceed4a
Michał Itrych <michal.itrych1312@gmail.com>
2024-05-09 15:25:01 +0200

    added authentication for dashboard

--- src/routes/dashboard.py
+++ src/routes/dashboard.py
@@ -1,18 +1,26 @@
-from flask import Blueprint, jsonify, request, Response
+from flask import Blueprint, jsonify, request
 from src.utils import connect_to_mongodb, close_mongodb_connection
 
 dashboard_bp = Blueprint('dashboard_bp', __name__)
 
 
-@dashboard_bp.route('/api/login', methods=['POST'])
-def login():
+@dashboard_bp.route('/api/authenticate', methods=['POST'])
+def authenticate():
     try:
+        auth_key = request.authorization
+        if not auth_key:
+            return jsonify({'error': 'Authentication credentials not provided'}), 401
+
         client, db = connect_to_mongodb()
         users = db.users
-        authKey = list(users.find({'authKey': str(request.authorization)}))
+        auth_user = users.find_one({'authKey': str(auth_key)})
         close_mongodb_connection(client)
-        if authKey:
-            return jsonify({'error': 'Authentication successful'}), 200
+
+        if auth_user:
+            if request.path == '/api/login':
+                return jsonify({'message': 'Login successful'}), 200
+            elif request.path == '/api/private':
+                return jsonify({'message': 'Private access granted'}), 200
         else:
             return jsonify({'error': 'Authentication failed'}), 401
     except Exception as e:
